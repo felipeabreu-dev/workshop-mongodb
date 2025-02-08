2cf71aada7345764e30481a919ab3532bd42b6cd
Felipe Abreu <abreufelipe12@hotmail.com>
2025-02-08 19:10:20 -0300

    Simple query with @Query

--- src/main/java/io/github/abreufelipedev/repositories/PostRepository.java
+++ src/main/java/io/github/abreufelipedev/repositories/PostRepository.java
@@ -2,10 +2,14 @@ package io.github.abreufelipedev.repositories;
 
 import io.github.abreufelipedev.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String title);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 }
